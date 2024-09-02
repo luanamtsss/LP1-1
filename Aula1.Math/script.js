20ac7d028e877701f8ccd3a855f8b690b7be5ee9
Jubaars <juliaalcassa692@gmail.com>
2024-09-02 08:45:58 -0300

    ex 1 math

---
+++ Aula1.Math/script.js
@@ -0,0 +1,26 @@
+let numero = 4.7
+
+// Math.around arredondamento comum
+console.log(Math.round(numero))
+
+// Math.ceil arredonda sempre para maior
+console.log(Math.ceil(numero))
+
+// Math.floor arredonda sempre para menor
+console.log(Math.floor(numero))
+
+// Math.pow gera uma potencia
+console.log(Math.pow(5, 2))
+
+// Math.sqrt gera uma raiz quadrada
+console.log(Math.sqrt(35))
+
+// Math.abs gera o número absoluto positivo
+console.log(Math.abs(-5))
+ 
+// Math.min e Math.max encontram o menor e maior número
+console.log(Math.min(7, 45, 89, 2, 125))
+console.log(Math.max(7, 45, 89, 2, 125))
+
+let numeroAleatorio = Math.floor()
+console.log(numeroAleatorio)
